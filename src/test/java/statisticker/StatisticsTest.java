30e5831a6cec3bb33cdc31cc6a77023587e4a456
kiran-kumar-r <61319940+kiran-kumar-r@users.noreply.github.com>
2020-10-12 02:11:36 +0530

    Update StatisticsTest.java

--- src/test/java/statisticker/StatisticsTest.java
+++ src/test/java/statisticker/StatisticsTest.java
@@ -15,7 +15,7 @@ public class StatisticsTest
     public void reportsAverageMinMaxx()
     {
         Float[] numbers = {1.5f, 8.9f, 3.2f, 4.5f};
-        List<___> numberList = Arrays.asList(numbers);
+        List<Float> numberList = Arrays.asList(numbers);
 
         Statistics.Stats s = Statistics.getStatistics(numberList);
 
@@ -27,9 +27,13 @@ public class StatisticsTest
     @Test
     public void reportsNaNForEmptyInput()
     {
-        List<___> emptyList = new ArrayList<___>();
+        List<Float> emptyList = new ArrayList<Float>();
 
         Statistics.Stats s = Statistics.getStatistics(emptyList);
+        
+        assertEquals(s.average.isNaN(),true);
+        assertEquals(s.min.isNaN(),true);
+        assertEquals(s.max.isNaN(),true);
 
         //All fields of computedStats (average, max, min) must be
         //Float.NaN (not-a-number), as described in
